32b3149f0af9d047d894315e7be692aebb8edb53
Rodrigo Segatto <segattorodrigo@hotmail.com>
2022-03-11 16:09:12 -0300

    Primeiro commit

---
+++ frontend/devweek/src/app/ocorrencias/ocorrencias/ocorrencias.component.ts
@@ -0,0 +1,20 @@
+import { RegiaoService } from './../service/regiao.service';
+import { Regiao } from './../model/regiao';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ocorrencias',
+  templateUrl: './ocorrencias.component.html',
+  styleUrls: ['./ocorrencias.component.css']
+})
+export class OcorrenciasComponent implements OnInit {
+
+  regioes: Regiao[] = [];
+
+  constructor(private regiaoService:RegiaoService) { }
+
+  ngOnInit(): void {
+    this.regioes = this.regiaoService.listRegioes();
+  }
+
+}
